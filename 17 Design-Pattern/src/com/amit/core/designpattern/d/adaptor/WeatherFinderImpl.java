63cb98386226998e6ef13a6a7da80b8fb2a49d3b
amitkvaio <46754383+amitkvaio@users.noreply.github.com>
2022-06-22 20:53:04 +0530

    Adding Design pattern example
    Adding Design pattern example

---
+++ 17 Design-Pattern/src/com/amit/core/designpattern/d/adaptor/WeatherFinderImpl.java
@@ -0,0 +1,10 @@
+package com.amit.core.designpattern.d.adaptor;
+
+public class WeatherFinderImpl implements WeatherFinder {
+
+	@Override
+	public int find(String city) {
+		return 34;
+	}
+
+}
---
+++ 17 Design-Pattern/src/com/amit/core/designpattern/f/immutable/StudentAddress01.java
@@ -0,0 +1,14 @@
+package com.amit.core.designpattern.f.immutable;
+
+public class StudentAddress01 {
+	public static void main(String[] args) {
+		Student st = new Student("Amit", "Kumar", new Address("Kadugodi", "WhiteFiled", "Bangalore"));
+		System.out.println("St Before :"+st);
+		Address ad = st.getAddress();
+		
+		ad.setFirstLine("Marathalli");
+		ad.setSecondLine("Kalamandir");
+		ad.setCity("Bangalore");
+		System.out.println("St After :"+st);
+	}
+}
